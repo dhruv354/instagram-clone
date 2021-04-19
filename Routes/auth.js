cb4418db62c957ef1c3d150816fa44043b941720
dhruv singhal <dhruv.singhal2612@gmail.com>
2021-04-19 13:33:50 +0530

    added bcrypt to secirely save passwords in the database

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
 router.get("/", (req, res) => {
   res.send("hello world");
@@ -10,6 +12,26 @@ router.post("/signup", (req, res) => {
   if (!name || !email || !password) {
     return res.status(422).json({ error: "please enter all the fields" });
   }
-  console.log(req.body.name);
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res
+          .status(422)
+          .json({ error: "user already exists in the database" });
+      }
+      //if user does not exist initially
+      bcrypt
+        .hash(password, 11)
+        .then((hashedPassword) => {
+          req.body.password = hashedPassword;
+          const user = new User(req.body);
+          user
+            .save()
+            .then((user) => res.json({ message: "successfully saved" }))
+            .catch((err) => res.send("Error", err));
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
 });
 module.exports = router;
